f1a191fb58ba18127d828038efba19baacef9463
“Krushang” <krushang.s19@iiits.in>
2019-10-19 19:19:44 +0530

    commit the death?

--- calc.c
+++ calc.c
@@ -1,4 +1,7 @@
 #include<stdio.h>
+float diff(int a, int b){
+    return a-b;
+}
 
 void main()
 {
@@ -6,6 +9,8 @@ void main()
     printf("Enter the numbers pesent ");
     scanf("%f %f",&a,&b);
     printf("the answer is 8; just  messing with ya.... its %f \n",a+b);
+    float c=diff(a,b);
+    printf("the diff is 8; just  messing with ya.... its %f \n",c);
 }
     
     
